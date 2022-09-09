6676a301fc03a9ab1c9f30933caf7400f43857bc
Citrine INS 04 <citrineins04@gmail.com>
2022-09-09 21:35:55 +0900

    Make Application

--- src/main/java/com/howlab/web/springboot/Main.java
+++ src/main/java/com/howlab/web/springboot/Main.java
@@ -1,4 +1,6 @@
-package com.howlab.web;public class Main {
+package com.howlab.web.springboot;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
